acfb09a177eebfaf47f174da5b0a4bf5603f94d8
jwlee <lemonade4813@gmail.com>
2024-05-26 23:26:54 +0900

    Initial commit

---
+++ src/test/java/com/example/travelappbackend/TravelAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
