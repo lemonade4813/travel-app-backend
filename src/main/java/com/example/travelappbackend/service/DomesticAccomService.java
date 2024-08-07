b989a1f6bd1c18ac6f6e5b08f3cf47ddb86184c8
jwlee <lemonade4813@gmail.com>
2024-08-08 00:41:56 +0900

    feat : 국내 숙박 리스트 URL 수정 적용.

--- src/main/java/com/example/travelappbackend/service/DomesticAccomService.java
+++ src/main/java/com/example/travelappbackend/service/DomesticAccomService.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,22 +30,23 @@ public class DomesticAccomService {
     }
 
     public void fetchAndSaveDomesticAccomItems() {
-
-        String url ="https://apis.data.go.kr/B551011/KorService1/searchStay1?serviceKey=GagNlrULGxksg16%2B71Pvi19nM5wOAy66KUlK5LF%2FfIXAe7fOeEPl3FyOBEJbnil91it6z5BSFNXDMxUMI9qEZg%3D%3D&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A";
-
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode;
-
         try {
-            rootNode = objectMapper.readTree(response.getBody());
+            String url = "https://apis.data.go.kr/B551011/KorService1/searchStay1?serviceKey=GagNlrULGxksg16%2B71Pvi19nM5wOAy66KUlK5LF%2FfIXAe7fOeEPl3FyOBEJbnil91it6z5BSFNXDMxUMI9qEZg%3D%3D&numOfRows=100&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A";
+
+            URI uri = new URI(url);
+
+            ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response.getBody());
             JsonNode itemsNode = rootNode.path("response").path("body").path("items").path("item");
 
             if (itemsNode.isArray()) {
-                List<Accom> accomsItems = Arrays.asList(objectMapper.convertValue(itemsNode, Accom[].class));
-                accomRepository.saveAll(accomsItems);
+                List<Accom> accomItems = Arrays.asList(objectMapper.convertValue(itemsNode, Accom[].class));
+                accomRepository.saveAll(accomItems);
             }
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
